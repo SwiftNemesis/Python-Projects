a65d7235523a61d5eba89087c9c5479ae1605f85
SwiftNemesis <bbsowers@gmail.com>
2022-10-07 16:12:06 -0700

    aaaaaa

--- discordBot/bot.py
+++ discordBot/bot.py
@@ -1,34 +1,22 @@
 import discord
-from discord.ext import commands
 import os
 import random
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 
 intents = discord.Intents.all()
-intents.messages = True
-bot = discord.Client(intents=intents)
-
-
 token = os.getenv('TOKEN')
 
-@bot.event
-async def on_ready():
-    guild_count = 0
+discord_bot = discord.Client(intents=intents)
+
+@discord_bot.event
+async def on_message(message):
+    if message.content == "hello":
+        await message.reply("hey dirtbag")
+
     
-    for guild in bot.guilds:
-            print(f" - {guild.id} (name: {guild.name})")
+discord_bot.run(token) # type: ignore
 
-            guild_count += 1
-            
-    print("FCC Reminder Bot is in " + str(guild_count) + " guilds.")
-
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-
-bot.run(token)
 
